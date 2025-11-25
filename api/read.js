abf328c6db8933540d5b72c4dc707d4cd887bf29
kalz <chnationg@gmail.com>
2025-11-25 19:30:10 +0700

    Delete api/read.js

--- api/read.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { login, domain, id } = req.query;
-
-  const data = await fetch(
-    `https://www.1secmail.com/api/v1/?action=readMessage&login=${login}&domain=${domain}&id=${id}`
-  ).then(r => r.json());
-
-  res.json({ ok: true, data });
-}
